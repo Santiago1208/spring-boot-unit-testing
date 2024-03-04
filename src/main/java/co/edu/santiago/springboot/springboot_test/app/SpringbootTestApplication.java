10c03d42479c7b71b84baf8b961d1e93c97c4a99
Santiago Restrepo <santiago.restrepop@perficient.com>
2024-03-03 23:00:05 -0500

    Spring Boot Service Testing starts

---
+++ src/main/java/co/edu/santiago/springboot/springboot_test/app/SpringbootTestApplication.java
@@ -0,0 +1,13 @@
+package co.edu.santiago.springboot.springboot_test.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootTestApplication.class, args);
+    }
+
+}
