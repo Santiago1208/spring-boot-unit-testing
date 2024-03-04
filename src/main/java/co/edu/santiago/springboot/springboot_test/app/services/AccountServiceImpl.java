a4fdb3cf167eb1d77f5e250a4eb5f08558ef90e7
Santiago Restrepo <santiago.restrepop@perficient.com>
2024-03-03 23:41:38 -0500

    Create implementation for AccountService interface

---
+++ src/main/java/co/edu/santiago/springboot/springboot_test/app/services/AccountServiceImpl.java
@@ -0,0 +1,52 @@
+package co.edu.santiago.springboot.springboot_test.app.services;
+
+import co.edu.santiago.springboot.springboot_test.app.models.Account;
+import co.edu.santiago.springboot.springboot_test.app.models.Bank;
+import co.edu.santiago.springboot.springboot_test.app.repository.AccountRepository;
+import co.edu.santiago.springboot.springboot_test.app.repository.BankRepository;
+
+import java.math.BigDecimal;
+
+public class AccountServiceImpl implements AccountService {
+    private AccountRepository accountRepository;
+    private BankRepository bankRepository;
+
+    public AccountServiceImpl(AccountRepository accountRepository, BankRepository bankRepository) {
+        this.accountRepository = accountRepository;
+        this.bankRepository = bankRepository;
+    }
+
+    @Override
+    public Account findById(Long id) {
+        return accountRepository.findById(id);
+    }
+
+    @Override
+    public int getTotalTransfers(Long bankId) {
+        Bank bank = bankRepository.findById(bankId);
+        return bank.getTotalTransfers();
+    }
+
+    @Override
+    public BigDecimal getBalance(Long id) {
+        Account account = accountRepository.findById(id);
+        return account.getBalance();
+    }
+
+    @Override
+    public void transfer(Long sourceAccountId, Long targetAccountId, BigDecimal amount) {
+        Account sourceAccount = accountRepository.findById(sourceAccountId);
+        Account targetAccount = accountRepository.findById(targetAccountId);
+
+        sourceAccount.debit(amount);
+        accountRepository.update(sourceAccount);
+
+        Bank bank = bankRepository.findById(1L); // Hard-coding the Bank ID because it should come from the user's session, out of the scope.
+        int totalTransfers = bank.getTotalTransfers();
+        bank.setTotalTransfers(++totalTransfers);
+        bankRepository.update(bank);
+
+        targetAccount.credit(amount);
+        accountRepository.update(targetAccount);
+    }
+}
