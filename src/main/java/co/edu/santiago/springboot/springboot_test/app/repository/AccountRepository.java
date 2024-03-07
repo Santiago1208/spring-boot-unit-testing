1fab008f06f8f5c6b2e6b20e4f197dc396ad49c0
Santiago Restrepo <santiago.restrepop@perficient.com>
2024-03-07 05:04:39 -0500

    Set up the JPA repositories, refactor

--- src/main/java/co/edu/santiago/springboot/springboot_test/app/repository/AccountRepository.java
+++ src/main/java/co/edu/santiago/springboot/springboot_test/app/repository/AccountRepository.java
@@ -1,11 +1,14 @@
 package co.edu.santiago.springboot.springboot_test.app.repository;
 
 import co.edu.santiago.springboot.springboot_test.app.models.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import java.util.Optional;
 
-public interface AccountRepository {
-    List<Account> findAll();
-    Account findById(Long id);
-    void update(Account account);
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Long> {
+    @Query("select acc from Account acc where acc.owner ilike %?1")
+    Optional<Account> findByOwnerStartingWith(String ownerName);
 }
--- src/main/java/co/edu/santiago/springboot/springboot_test/app/repository/BankRepository.java
+++ src/main/java/co/edu/santiago/springboot/springboot_test/app/repository/BankRepository.java
@@ -1,12 +1,11 @@
 package co.edu.santiago.springboot.springboot_test.app.repository;
 
 import co.edu.santiago.springboot.springboot_test.app.models.Bank;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-public interface BankRepository {
-    List<Bank> findAll();
-    Bank findById(Long id);
-    void update(Bank bank);
-
+@Repository
+public interface BankRepository extends JpaRepository<Bank, Long> {
 }
