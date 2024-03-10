53abaa969e0ce65a862d6efd3a88b071f3018eb9
Santiago Restrepo <santiago.restrepop@perficient.com>
2024-03-10 18:33:16 -0500

    Configure test to use the local controllers and consumeWith, expectBody vs jsonPath exploration

--- src/test/java/co/edu/santiago/springboot/springboot_test/app/controllers/AccountControllerWTCTest.java
+++ src/test/java/co/edu/santiago/springboot/springboot_test/app/controllers/AccountControllerWTCTest.java
@@ -1,6 +1,7 @@
 package co.edu.santiago.springboot.springboot_test.app.controllers;
 
 import co.edu.santiago.springboot.springboot_test.app.models.dto.TransferDTO;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -10,8 +11,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import java.io.IOException;
 import java.math.BigDecimal;
-import java.net.URI;
 import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
@@ -48,14 +49,26 @@ class AccountControllerWTCTest {
         assertDoesNotThrow(() -> {
             webClient
                     .post()
-                    .uri(URI.create("http://localhost:8080/api/accounts/transfer"))
+                    .uri("/api/accounts/transfer")
                     .contentType(APPLICATION_JSON)
                     .bodyValue(transferDTO)
                     .exchange()
                     // Then
                     .expectStatus().isOk()
-                    .expectBody()
-                    .jsonPath("$.message").isNotEmpty()
+                    .expectBody() // By default, it returns byte[]. You can call expectBody(String.class) to get the String return but it won't be compatible with jsonPath()
+                    .consumeWith(serverResponse -> { // Use this for the case you used expectBody(String.class)
+                        try {
+                            byte[] body = serverResponse.getResponseBody();
+                            JsonNode json = mapper.readTree(body);
+                            assertEquals("Transfer performed successfully", json.path("message").asText());
+                            assertEquals(1L, json.path("transaction").path("sourceAccountId").asLong());
+                            assertEquals("100", json.path("transaction").path("amount").asText());
+                            assertEquals(LocalDate.now().toString(), json.path("date").asText());
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    })
+                    .jsonPath("$.message").isNotEmpty() // Or use this when using the default byte[]
                     .jsonPath("$.message").isEqualTo("Transfer performed successfully")
                     .jsonPath("$.transaction.sourceAccountId").isEqualTo(transferDTO.sourceAccountId())
                     .jsonPath("$.date").isEqualTo(LocalDate.now().toString())
